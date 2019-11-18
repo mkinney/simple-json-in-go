e8d3445b9f333e088194859aa4045dc1f10ad522
Mike Kinney <mike.kinney@gmail.com>
2019-11-18 11:39:29 -0800

    experiment with coverage

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 	"testing"
+	"strings"
 )
 
 func TestDecode(t *testing.T) {
@@ -17,6 +18,40 @@ func TestDecode(t *testing.T) {
 		t.Error("Wrong content. Was expecting 'Hello World!' but got ", post.Content)
 	}
 }
+
+// helper function
+// https://stackoverflow.com/questions/42035104/how-to-unit-test-go-errors
+// which looks to be from https://github.com/Teamwork/test
+func ErrorContains(out error, want string) bool {
+	if out == nil {
+		return want == ""
+	}
+	if want == "" {
+		return false
+	}
+	return strings.Contains(out.Error(), want)
+}
+
+func TestDecodeInvalidFile(t *testing.T) {
+	_, err := decode("not_really_a_file.json")
+	if !ErrorContains(err, "no such file or directory") {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	post, err := unmarshal("post.json")
+	if err != nil {
+		t.Error(err)
+	}
+	if post.Id != 1 {
+		t.Error("Wrong id. Was expecting 1 but got ", post.Id)
+	}
+	if post.Content != "Hello World!" {
+		t.Error("Wrong content. Was expecting 'Hello World!' but got ", post.Content)
+	}
+}
+
 
 func TestEncode(t *testing.T) {
 	t.Skip("Skipping encoding for now")
